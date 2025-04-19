c60b91d14d679cbbe92ea43a528c3fd10bff818f
SAFD <69416816+ohxorud-dev@users.noreply.github.com>
2025-04-19 10:45:20 +0900

    Update install-user.sh

--- install-user.sh
+++ install-user.sh
@@ -3,7 +3,7 @@
 echo "### Installing yay ###"
 git clone https://aur.archlinux.org/yay.git /tmp/yay
 cd /tmp/yay
-makepkg -si
+makepkg -si --noconfirm
 
 echo "### Installing Additional packages ###"
 yay -S --noconfirm --needed hyprland waybar hyprpaper kitty brightnessctl pavucontrol polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland wl-clipboard network-manager-applet fcitx5 fcitx5-qt fcitx5-gtk fcitx5-hangul fcitx5-configtool man openssh pipewire pipewire-jack pipewire-alsa pipewire-pulse zen-browser-bin gitkraken timeshift-auto sddm nerd-fonts ttf-fira-code fish wofi starship pamixer swaync power-profiles-daemon jetbrains-toolbox uwsm hyprpolkitagent
